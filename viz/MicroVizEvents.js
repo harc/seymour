6b05e06bfe63ef9da9786d4b00f929b3b67d47fd
Alessandro Warth <alexwarth@gmail.com>
2017-06-21 14:07:35 -0700

    fix remote ... bug

--- viz/MicroVizEvents.js
+++ viz/MicroVizEvents.js
@@ -15,7 +15,10 @@ class MicroVizEvents extends CheckedEmitter {
   }
 
   add(event) {
-    const eventIsLocal = this.sourceLoc.strictlyContains(event.sourceLoc);
+    // TODO: make sure that `contains` is the right thing -- it used to be strictlyContains,
+    // but that was causing problems for programs that were a single variable declaration, e.g.,
+    // `var x = 2;`
+    const eventIsLocal = this.sourceLoc.contains(event.sourceLoc);
     if (eventIsLocal && this.lastEventGroup instanceof LocalEventGroup) {
       const lastEvent = this.lastEventGroup.lastEvent;
       if (event.sourceLoc.startPos >= lastEvent.sourceLoc.endPos ||  // Toby's rule
