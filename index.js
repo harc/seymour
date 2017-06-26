d0bbae9a5aaeba7add47b0786304e4e3b7d695cf
Alessandro Warth <alexwarth@gmail.com>
2017-06-26 01:10:49 -0700

    bug fix + styling before demo

--- index.js
+++ index.js
@@ -314,7 +314,11 @@ editor.on('changes', function(cmInstance, changes) {
     const error = document.createElement('parseError');
     error.innerText = spaces(pos.ch) + '^\nExpected: ' + expected;
     parseErrorWidget = editor.addLineWidget(pos.line, error);
-    $(error).hide().delay(2000).slideDown().queue(() => parseErrorWidget.changed());
+    $(error).hide().delay(2000).slideDown().queue(() => {
+      if (parseErrorWidget) {
+        parseErrorWidget.changed();
+      }
+    });
     parseErrorWidget.changed();
   }
 });
